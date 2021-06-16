84c4cccf3a48b69237bc4708444f5c64ce8fe694
Jyosh-123 <69143816+Jyosh-123@users.noreply.github.com>
2021-06-16 19:46:53 +0530

    Add files via upload

---
+++ JP LAB/Threads/Priorityandname.java
@@ -0,0 +1,8 @@
+class Priorityandname
+{
+public static void main (String args[])
+{
+System.out.println("Thread Name = " + Thread.currentThread().getName());
+System.out.println(" Priority = " + Thread.currentThread().getPriority());
+}
+} 
